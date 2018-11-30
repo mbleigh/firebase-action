869bf57b297be6879d9a10c27e667f4e07e1a95c
Michael Bleigh <bleigh@google.com>
2018-11-30 10:51:44 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
-if [ -z "$ENCODED_SERVICE_ACCOUNT" ]; then
+if [ -n "$ENCODED_SERVICE_ACCOUNT" ]; then
+  echo ">> Detected encoded service account. Decoding to /service_account.json"
   echo "$ENCODED_SERVICE_ACCOUNT" | base64 --decode > /service_account.json
   export GOOGLE_APPLICATION_CREDENTIALS=/service_account.json
 fi
