642d2e6cf1af80f9324f508393e2c93965ecd9c5
Michael Bleigh <bleigh@google.com>
2018-12-11 09:33:23 -0800

    Allow for variable credential key.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 
-if [ -n "$ENCODED_SERVICE_ACCOUNT" ]; then
-  echo ">> Detected encoded service account. Decoding to /service_account.json"
-  echo "$ENCODED_SERVICE_ACCOUNT" | base64 --decode > /service_account.json
+if [ -n "$ENCODED_APPLICATION_CREDENTIALS_KEY" ]; then
+  echo ">> Using value stored in '$ENCODED_APPLICATION_CREDENTIALS_KEY' as encoded service account key"
+  export ENCODED_APPLICATION_CREDENTIALS=${!ENCODED_APPLICATION_CREDENTIALS_KEY}
+fi
+
+if [ -n "$ENCODED_APPLICATION_CREDENTIALS" ]; then
+  echo ">> Detected encoded service account credential. Decoding to /service_account.json"
+  echo "$ENCODED_APPLICATION_CREDENTIALS" | base64 --decode > /service_account.json
   export GOOGLE_APPLICATION_CREDENTIALS=/service_account.json
 fi
 
